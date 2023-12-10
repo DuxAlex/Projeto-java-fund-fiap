09d0d2ff0106bc11ad42324ed5c6bbb32333393a
DuxAlex <alexkrypto.ti@gmail.com>
2023-12-10 02:45:18 -0300

    trabalhando com Strings, concatenação, escapes e etc

--- src/teste/br/com/fiap/estrturasarraysetc/Caixa.java
+++ src/teste/br/com/fiap/estrturasarraysetc/Caixa.java
@@ -54,25 +54,85 @@ public class Caixa {
 		for (Produto prod : produtos) {
 			System.out.println(prod.toString());
 		}
-		
-		//array multidimensional
-		Produto [] [] localizacaoProduto = new Produto [10][3];
-		
-		
-		//limao fica no indice 0 posicao 1
-		//maçã fica no indice 1 posicao 1
-		localizacaoProduto [0] [1] = prod1;
-		localizacaoProduto [1] [1] = prod2;
-		
-		System.out.println(localizacaoProduto [0][1].getNome());
-		
-		
-		
-		
-		
-		
-		
-		
 
+		// array multidimensional
+		Produto[][] localizacaoProduto = new Produto[10][3];
+
+		// limao fica no indice 0 posicao 1
+		// maçã fica no indice 1 posicao 1
+		localizacaoProduto[0][1] = prod1;
+		localizacaoProduto[1][1] = prod2;
+
+		System.out.println(localizacaoProduto[0][1].getNome());
+
+		// Trabalhando com Strings
+
+		String nome;
+		nome = new String();
+		nome = "maça";
+
+		System.out.println(nome);
+
+		String nome2 = new String("Maçã");
+		System.out.println(nome2);
+
+		String nome3 = "maçã";
+		System.out.println(nome3);
+
+	
+		//utilizando escapes
+		
+		
+		//pular linha e posiciona o cursor no inicio da segudna linha
+		String descricao = "tipo gala \nA maçã mais doce do mercado";
+		System.out.println(descricao);
+		
+		
+		//dar um espaço maior
+		descricao = "tipo gala \tA mação masi doce do mercado";
+		System.out.println(descricao);
+		
+		
+		//utilizar aspas como caractere
+		descricao = "tipo gala: \"A maçã mais doce do mercado\"";
+		System.out.println(descricao);
+	
+		
+		//concatenar
+		
+		String propaganda = nome + " " + descricao;
+		System.out.println(propaganda);
+		
+		propaganda = nome.concat(" ").concat(descricao);
+		System.out.println(propaganda);
+		
+		propaganda += "!";
+		System.out.println(propaganda);
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
